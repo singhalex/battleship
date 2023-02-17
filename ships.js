ad3c28ae9da7db185d9e8ebe087a113786e28152
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-16 22:00:27 -0800

    Add a hit method to the ship
    Method increases the number of hits on the ship

--- ships.js
+++ ships.js
@@ -1,6 +1,11 @@
 export const createShip = (length) => {
     let hits = 0;
     const getHits = () => hits;
+    const hit = () => {
+        hits += 1;
+    }
+
+
     const isSunk = 1;
-    return { length, getHits}
+    return { length, getHits, hit}
 }
--- ships.test.js
+++ ships.test.js
@@ -9,3 +9,15 @@ test('Check the hits on a ship', () => {
     const getHitsShip = createShip(5);
     expect(getHitsShip.getHits()).toBe(0)
 })
+
+test('Add a hit to ship and expect number of hits to be 1', () => {
+    const hitTheShip = createShip(5);
+    hitTheShip.hit();
+    expect(hitTheShip.getHits()).toBe(1);
+})
+test('Add 2 hits to ship and expect number of hits to be 2', () => {
+    const hitTheShip = createShip(5);
+    hitTheShip.hit();
+    hitTheShip.hit();
+    expect(hitTheShip.getHits()).toBe(2);
+})
