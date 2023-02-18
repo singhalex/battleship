5c495dad3ea290a9dd29b2e6579d232473b153b5
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-17 22:56:40 -0800

    Add placeShip method to gameBoard
    Creates a ship object and places it on the board with the given
    coordinates

--- src/factories/boards.js
+++ src/factories/boards.js
@@ -1,3 +1,5 @@
+import { createShip } from "./ships";
+
 export const createBoard = () => {
     const width = 10;
     const height = 10;
@@ -7,9 +9,16 @@ export const createBoard = () => {
         gameBoard[i] = new Array(height)
     }
 
+    const placeShip = (length, x, y) => {
+        const fiveShip = createShip(length);
+        for (let i = 0; i < length; i++) {
+            gameBoard[x + i][y] = fiveShip;
+        }
+    }
+
 
     return {
-        gameBoard
+        gameBoard, placeShip
     }
 
 }
--- src/factories/boards.test.js
+++ src/factories/boards.test.js
@@ -1,9 +1,10 @@
 import { createBoard } from "./boards";
+import { createShip } from "./ships";
 
 test('Game board should have a width of 10', () => {
     const board = createBoard();
     expect(board.gameBoard.length).toBe(10)
-})
+});
 
 test('Every column of game board should have a height of 10', () => {
     const board = createBoard();
@@ -11,4 +12,31 @@ test('Every column of game board should have a height of 10', () => {
     for (let i = 0; i < board.gameBoard.length; i++) {
         expect(board.gameBoard[i].length).toBe(10)
     }
-})
+});
+
+test('Place a 5 length ship at coordinates [0,0] horizontally', () => {
+    const length = 5;
+    const x = 0;
+    const y = 0;
+    const board = createBoard();
+    board.placeShip(length, x, y);
+    const fiveShip = createShip(5);
+
+    expect(board.gameBoard[x][y].length).toBe(fiveShip.length)
+    expect(board.gameBoard[x + 1][y].length).toBe(fiveShip.length)
+    expect(board.gameBoard[x + 2][y].length).toBe(fiveShip.length)
+    expect(board.gameBoard[x + 3][y].length).toBe(fiveShip.length)
+    expect(board.gameBoard[x + 4][y].length).toBe(fiveShip.length)
+});
+
+test('Place a 2 length ship at coordinates [0,0] horizontally', () => {
+    const length = 2;
+    const x = 0;
+    const y = 0;
+    const board = createBoard();
+    board.placeShip(length, x, y);
+    const fiveShip = createShip(2);
+
+    expect(board.gameBoard[x][y].length).toBe(fiveShip.length)
+    expect(board.gameBoard[x + 1][y].length).toBe(fiveShip.length)
+});
