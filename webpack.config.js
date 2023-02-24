b532dc27496c76f4eb7acc8c4a182ad84fe705ac
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-23 18:27:47 -0800

    Add html plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
