0caa7067846d42b09c879e0851023d7e054a068d
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-26 22:37:23 -0800

    Add button to rotate ships in ship container

--- src/ui.js
+++ src/ui.js
@@ -4,21 +4,38 @@ export const userInterface = () => {
         for (let x = 0; x < player.board.grid.length; x++) {
             for (let y = 0; y < player.board.grid[x].length; y++) {
                 const square = document.createElement('div');
-                square.className = 'square';
                 square.dataset.x = x;
                 square.dataset.y = y;
-                square.addEventListener('click', () => {
-                    if (player.board.ships.length !== 0) {
-                        player.board.placeShip(x, y)
-                        console.log(player.board.ships)
-                    }
-                    console.log(square.dataset.x)
-                    console.log(square.dataset.y)
-                })
+
+                square.className = 'square';
+                if (player.name === 'CPU') {
+                    square.classList.add('hoverable')
+                    square.addEventListener('click', () => {
+                        if (player.board.ships.length !== 0) {
+                            player.board.placeShip(x, y)
+                            console.log(player.name)
+                            console.log(player.board.ships)
+                        }
+                        console.log(square.dataset.x)
+                        console.log(square.dataset.y)
+                    })
+                }
                 container.appendChild(square);
             }
         }
     }
+
+    
+
+    const rotateButton = document.querySelector('#rotate');
+    const ships = document.querySelector('#ships').children
+    let degrees = 0;
+    rotateButton.addEventListener('click', () => {
+        degrees = degrees === 0 ? 90 : 0;
+        for (const ship of ships) {
+            ship.style.transform = `rotate(${degrees}deg)`
+        }
+    })
 
     return { buildGrid }
 }
